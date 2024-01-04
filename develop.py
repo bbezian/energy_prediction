dd24c44eadeab0a8a0cdcfb302ce59cf5a37b7b3
bbezian <bbezian@yahoo.com>
2024-01-04 10:18:36 -0500

    edit code

--- develop.py
+++ develop.py
@@ -1,57 +1,27 @@
 import streamlit as st
+import os
 import joblib 
 import numpy as np 
 import pandas as pd
 
-load_scaler = joblib.load('scaler.pkl')
-load_scaler_y = joblib.load('scaler_y.pkl')
-load_model = joblib.load('energy_prediction_model.pkl')
+load_scaler_file_path = os.path.abspath('scaler.pkl')
+load_scaler_y_file_path = os.path.abspath('scaler_y.pkl')
+load_model_file_path = os.path.abspath('energy_prediction_model.pkl')
+
+load_scaler = joblib.load(load_scaler_file_path)
+load_scaler_y = joblib.load(load_scaler_y_file_path)
+load_model = joblib.load(load_model_file_path)
 
 data = pd.read_csv('data.csv')#,index_col=0
-
 
 st.title('Energy prediction')
 st.write('''We need outdoor enthalpy to predict the energy consumption''')
 
 
 enthalpy = st.number_input("Enter your enthalpy value")
+st.subheader(f'Measured enthalpy value : {enthalpy: .4f}')
 
 
-
-# Title of the app
-st.title('Upload CSV File')
-
-# Upload a CSV file
-uploaded_file = st.file_uploader("data.csv", type=["csv"])
-
-# Check if a file was uploaded
-if uploaded_file is not None:
-    # Read the CSV file
-    df = pd.read_csv(uploaded_file)
-
-    # Display the DataFrame
-    st.write("### Displaying the DataFrame:")
-    st.write(df)
-
-
-    # Allow the user to select a row
-    selected_row = st.text_input("Select a date")
-    # Display the selected row
-    #st.write(df.iloc[selected_row])
-    selected_row = df[df['date']==str(selected_row)]
-    st.write(selected_row)
-
-
-    # Allow the user to select a column from the selected row
-    selected_column = st.selectbox("Select a column", df.columns)
-
-    # Display the selected column value from the selected row
-    selected_value = selected_row[selected_column].iloc[0]
-    st.write(f"{selected_column}: {selected_value}")
-
-
-
-enthalpy = selected_row['enthalpy'].iloc[0]    
 X = enthalpy
 X_tst = load_scaler.transform([[X]])
 
@@ -63,11 +33,12 @@ energy_predicted = energy_predicted.reshape(len(energy_predicted),1)
 
 st.subheader(f'The predicted energy consumption is : {float(energy_predicted):.4f}')
 
-measured_energy_consumption = selected_row['energy'].iloc[0]  
+measured_energy_consumption = st.number_input('Enter measured energy consumption')
 st.subheader(f'Measured energy consumption : {measured_energy_consumption: .4f}')
 
 
-Threshold = 306.62016807
+#Threshold = 306.62016807
+Threshold = st.number_input('Enter specified threshold')
 st.subheader(f'Threshold for abnormal energy consumption : {Threshold: .4f}')
 
 
